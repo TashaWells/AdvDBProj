d2b55972a8f033617ebff45a78e5bb0daf204836
TashaWells <34165614+TashaWells@users.noreply.github.com>
2017-12-09 15:54:44 -0500

    Add files via upload

---
+++ CourseManager/CourseManager/CourseViewer.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CourseManager
+{
+    partial class CourseViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DepartmentList = new System.Windows.Forms.ComboBox();
+            this.CloseForm = new System.Windows.Forms.Button();
+            this.courseGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DepartmentList
+            // 
+            this.DepartmentList.FormattingEnabled = true;
+            this.DepartmentList.Location = new System.Drawing.Point(26, 27);
+            this.DepartmentList.Name = "DepartmentList";
+            this.DepartmentList.Size = new System.Drawing.Size(502, 39);
+            this.DepartmentList.TabIndex = 0;
+            this.DepartmentList.SelectedIndexChanged += new System.EventHandler(this.DepartmentList_SelectedIndexChanged);
+            // 
+            // CloseForm
+            // 
+            this.CloseForm.Location = new System.Drawing.Point(782, 19);
+            this.CloseForm.Name = "CloseForm";
+            this.CloseForm.Size = new System.Drawing.Size(153, 53);
+            this.CloseForm.TabIndex = 1;
+            this.CloseForm.Text = "Close";
+            this.CloseForm.UseVisualStyleBackColor = true;
+            this.CloseForm.Click += new System.EventHandler(this.CloseForm_Click);
+            // 
+            // courseGridView
+            // 
+            this.courseGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.courseGridView.Location = new System.Drawing.Point(26, 148);
+            this.courseGridView.Name = "courseGridView";
+            this.courseGridView.RowTemplate.Height = 40;
+            this.courseGridView.Size = new System.Drawing.Size(920, 639);
+            this.courseGridView.TabIndex = 2;
+            // 
+            // CourseViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1508, 850);
+            this.Controls.Add(this.courseGridView);
+            this.Controls.Add(this.CloseForm);
+            this.Controls.Add(this.DepartmentList);
+            this.Name = "CourseViewer";
+            this.Text = "Course Viewer";
+            this.Load += new System.EventHandler(this.CourseViewer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox DepartmentList;
+        private System.Windows.Forms.Button CloseForm;
+        private System.Windows.Forms.DataGridView courseGridView;
+    }
+}
+
---
+++ CourseManager/CourseManager/CourseViewer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace CourseManager
+{
+    public partial class CourseViewer : Form
+    {
+        //Create an ObjectContext instance based on SchoolEntity
+        private SchoolEntities schoolContext;
+
+        public CourseViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseForm_Click(object sender, EventArgs e)
+        {
+            //Close the form
+            this.Close();
+        }
+
+        private void CourseViewer_Load(object sender, EventArgs e)
+        {
+            //Initialize the ObjectContext
+            schoolContext = new SchoolEntities();
+
+            // Define a query that returns all Department  
+            // objects and course objects, ordered by name.
+            var departmentQuery = from d in schoolContext.Departments.Include("Courses")
+                                  orderby d.Name
+                                  select d;
+            try
+            {
+                // Bind the ComboBox control to the query, 
+                // which is executed during data binding.
+                // To prevent the query from being executed multiple times during binding, 
+                // it is recommended to bind controls to the result of the Execute method. 
+                this.DepartmentList.DisplayMember = "Name";
+                //this.departmentList.DataSource = ((ObjectQuery)departmentQuery).Execute(MergeOption.AppendOnly);
+                this.DepartmentList.DataSource = departmentQuery.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void DepartmentList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                //Get the object for the selected department.
+                Department department = (Department)this.DepartmentList.SelectedItem;
+
+                //Bind the grid view to the collection of Course objects
+                // that are related to the selected Department object.
+                courseGridView.DataSource = department.Courses.ToList();
+
+                // Hide the columns that are bound to the navigation properties on Course.
+                courseGridView.Columns["Department"].Visible = false;
+                courseGridView.Columns["StudentGrades"].Visible = false;
+                courseGridView.Columns["OnlineCourse"].Visible = false;
+                courseGridView.Columns["OnsiteCourse"].Visible = false;
+                courseGridView.Columns["People"].Visible = false;
+                courseGridView.Columns["DepartmentId"].Visible = false;
+
+                courseGridView.AllowUserToDeleteRows = false;
+                courseGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
